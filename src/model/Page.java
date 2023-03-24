dfaffdadc2c550487709dfe0f407e52cc5effe92
Igors Saters <s21sateigor@venta.lv>
2023-03-24 14:25:53 +0200

    Edit: added getters/setters and constructors

--- src/model/Page.java
+++ src/model/Page.java
@@ -9,26 +9,66 @@ import java.util.ArrayList;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Page {
-    String title;
-    String description;
-    ArrayList<User> followers;
-    ArrayList<Post> postsInPage;
+    private String title;
+    private String description;
+    private ArrayList<User> followers = new ArrayList<>();
+    private ArrayList<Post> postsInPage = new ArrayList<>();
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setTitle(String inputTitle) {
+        if(inputTitle!=null && inputTitle.length() > 4 && inputTitle.length() < 31){
+            title = inputTitle;
+        } else {
+            title = " --TITLE-- ";
+        }
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String inputDescription) {
+        if (inputDescription != null && inputDescription.length() > 10 && inputDescription.length() < 31) {
+            description = inputDescription;
+        } else {
+            description = " --DESCRIPTION-- ";
+        }
     }
 
+
+    public ArrayList<User> getFollowers(){
+        return followers;
+    }
+
+    public void addFollower(User follower){
+        if(!followers.contains(follower)){
+            followers.add(follower);
+        }
+    }
+
+    public void removeFollower(User unFollower){
+        if(unFollower!=null && followers.contains(unFollower)){
+            followers.remove(unFollower);
+        }
+    }
+
+    public ArrayList<Post> getPostsInPage() {
+        return postsInPage;
+    }
+
+    public void addPostInPage(Post post){
+        if(post!=null){
+            postsInPage.add(post);
+        }
+    }
+
+    public void removePostFromPage(Post post){
+        if(post!=null && postsInPage.contains(post)){
+            postsInPage.remove(post);
+        }
+    }
 
 }
